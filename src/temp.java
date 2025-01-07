2db30754a97d3446ce2629adf36968dc8e2b65ff
TalyaMiara <taliyam123@gmail.com>
2025-01-07 13:24:41 +0200

    update

---
+++ src/temp.java
@@ -0,0 +1,17 @@
+public class temp {
+    public boolean isNumber(String text){
+        //try and catch, נרצה לבדוק אם הסטרינג שמוזן הוא מספר
+    }
+    public boolean isText(String text){
+        //אם הוא לא פורמולה ולא נמבר- הוא טקסט.
+    }
+    public boolean isForm(String text){
+        //בדיקה ש-text הוא לא  null
+        //בדיקה אם הוא לא empty
+        //חייב להתחיל בשווה- return false
+        //
+    }
+    public double computeForm(String form){
+
+    }
+}
